100eed51bff8fbb5e1a4697a8ce909fee98bc6c4
Deepakjhajhria <deepakjhajhria4657@gmail.com>
2023-09-23 12:01:08 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,10 @@ let span1 = document.querySelector(".span1")
 let span2 = document.querySelector(".span2")
 let span3 = document.querySelector(".span3")
 let home = document.querySelector(".home")
+let Aboutus = document.querySelector(".Aboutus")
+let Destinations = document.querySelector(".Destinations")
+let Tours = document.querySelector(".Tours")
+let Blog = document.querySelector(".Blog")
 
 menu.addEventListener("click", function () {
     mobileView.classList.toggle("show");
@@ -14,6 +18,34 @@ menu.addEventListener("click", function () {
     span3.classList.toggle("span6");
 })
 home.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
+})
+Aboutus.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
+})
+Destinations.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
+})
+Tours.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
+})
+Blog.addEventListener("click", function () {
     mobileView.classList.remove("show");
     body.classList.remove("overflow-hidden");
     span1.classList.remove("span4");
