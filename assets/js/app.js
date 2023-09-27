cc14d13a205a1374f8b2fdccb672724997bb8943
Deepakjhajhria <deepakjhajhria4657@gmail.com>
2023-09-27 11:48:21 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,44 +11,64 @@ let Tours = document.querySelector(".Tours")
 let Blog = document.querySelector(".Blog")
 
 menu.addEventListener("click", function () {
-    mobileView.classList.toggle("show");
-    body.classList.toggle("overflow-hidden");
-    span1.classList.toggle("span4");
-    span2.classList.toggle("span5");
-    span3.classList.toggle("span6");
+  mobileView.classList.toggle("show");
+  body.classList.toggle("overflow-hidden");
+  span1.classList.toggle("span4");
+  span2.classList.toggle("span5");
+  span3.classList.toggle("span6");
 })
 home.addEventListener("click", function () {
-    mobileView.classList.remove("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.remove("span4");
-    span2.classList.remove("span5");
-    span3.classList.remove("span6");
+  mobileView.classList.remove("show");
+  body.classList.remove("overflow-hidden");
+  span1.classList.remove("span4");
+  span2.classList.remove("span5");
+  span3.classList.remove("span6");
 })
 Aboutus.addEventListener("click", function () {
-    mobileView.classList.remove("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.remove("span4");
-    span2.classList.remove("span5");
-    span3.classList.remove("span6");
+  mobileView.classList.remove("show");
+  body.classList.remove("overflow-hidden");
+  span1.classList.remove("span4");
+  span2.classList.remove("span5");
+  span3.classList.remove("span6");
 })
 Destinations.addEventListener("click", function () {
-    mobileView.classList.remove("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.remove("span4");
-    span2.classList.remove("span5");
-    span3.classList.remove("span6");
+  mobileView.classList.remove("show");
+  body.classList.remove("overflow-hidden");
+  span1.classList.remove("span4");
+  span2.classList.remove("span5");
+  span3.classList.remove("span6");
 })
 Tours.addEventListener("click", function () {
-    mobileView.classList.remove("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.remove("span4");
-    span2.classList.remove("span5");
-    span3.classList.remove("span6");
+  mobileView.classList.remove("show");
+  body.classList.remove("overflow-hidden");
+  span1.classList.remove("span4");
+  span2.classList.remove("span5");
+  span3.classList.remove("span6");
 })
 Blog.addEventListener("click", function () {
-    mobileView.classList.remove("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.remove("span4");
-    span2.classList.remove("span5");
-    span3.classList.remove("span6");
+  mobileView.classList.remove("show");
+  body.classList.remove("overflow-hidden");
+  span1.classList.remove("span4");
+  span2.classList.remove("span5");
+  span3.classList.remove("span6");
 })
+
+let mybutton = document.getElementById("btn-back-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click", backToTop);
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
